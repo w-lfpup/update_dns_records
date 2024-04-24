d78a522cad297f825d88f25248c60c0804db5cbb
Taylor Vann <brian.t.vann@gmail.com>
2024-04-24 16:41:08 -0700

    correlate with features

--- domain_services/src/lib.rs
+++ domain_services/src/lib.rs
@@ -13,9 +13,8 @@ pub async fn update_domains(
     let mut domain_results = HashMap::<String, DomainResult>::new();
 
     // add more services here
-    
-    domain_results = dyndns2::update_domains(domain_results, results, config.domain_services.dyndns2).await;
-    domain_results = cloudflare::update_domains(domain_results, results, config.domain_services.cloudflare).await;
+    // domain_results = dyndns2::update_domains(domain_results, results, config.domain_services.dyndns2).await;
+    // domain_results = cloudflare::update_domains(domain_results, results, config.domain_services.cloudflare).await;
 
     domain_results
 }
