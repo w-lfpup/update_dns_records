aa5e85ef15f914f27a365ff5883cd77a3512edff
Taylor Vann <brian.t.vann@gmail.com>
2024-04-24 17:44:41 -0700

    features build

--- domain_services/src/lib.rs
+++ domain_services/src/lib.rs
@@ -8,22 +8,6 @@ use cloudflare;
 #[cfg(feature = "dyndns2")]
 use dyndns2;
 
-#[cfg(feature = "dyndns2")]
-async fn add_dyndns2_to_domain_results(
-    config: &Config,
-    doman_results: &mut HashMap<String, DomainResult>,
-) -> () {
-    dyndns2::update_domains(domain_results, results, config.domain_services.dyndns2).await;
-}
-
-#[cfg(feature = "cloudflare")]
-async fn add_cloudflare_to_domain_results(
-    config: &Config,
-    doman_results: &mut HashMap<String, DomainResult>,
-) -> () {
-    dyndns2::update_domains(domain_results, results, config.domain_services.dyndns2).await;
-}
-
 pub async fn update_domains(
     config: &Config,
     results: &UpdateIpResults,
@@ -31,10 +15,15 @@ pub async fn update_domains(
     let mut domain_results = HashMap::<String, DomainResult>::new();
 
     // add more services here
-    #[cfg(feature = "dyndns2")]
-    add_dyndns2_to_domain_results(config, results);
-    #[cfg(feature = "cloudflare")]
-    add_cloudflare_to_domain_results(config, results);
+    if cfg!(feature = "dyndns2") {
+        domain_results =
+            dyndns2::update_domains(domain_results, results, &config.domain_services.dyndns2).await;
+    }
+    if cfg!(feature = "cloudflare") {
+        domain_results =
+            cloudflare::update_domains(domain_results, results, &config.domain_services.cloudflare)
+                .await;
+    }
 
     domain_results
 }
